e9123f03f509adfae190aafadcfe4b1f937ea631
javm <quetzal1910@gmail.com>
2018-10-16 11:25:06 -0500

    Creation of hash with uniprot ids

--- cleaning_unclass_py/clean_unclass.py
+++ cleaning_unclass_py/clean_unclass.py
@@ -3,7 +3,13 @@ import re
 ############################ Open and write files ##############################
 
 uniprot_db = open("uniprot.txt", "r")
+
+unclassified = open("unclassified_set.txt", "r")
+
 uniprot_lines = open('uniprot_lines.txt','w')
+
+
+
 #
 # A1 = []
 # A2 = []
@@ -18,6 +24,19 @@ uniprot_lines = open('uniprot_lines.txt','w')
 # print(A1[0])
 
 # data
+
+unclassified_lines = unclassified.readlines()
+unclassified_ids = {}
+
+for line in unclassified_lines:
+     parts = line.split()
+     cols = parts[6].split('_')
+     if (len(cols) > 1):
+          uniprot_id = cols[1]
+          unclassified_ids[parts[0]] = uniprot_id.split('^')[0]
+
+print unclassified_ids
+
 uniprot_db_lines = uniprot_db.readlines()
 #print uniprot_db_lines
 
@@ -28,16 +47,16 @@ for i in range(1 , len(uniprot_db_lines)):
      line1 = uniprot_db_lines[i]
      line1 = line1.strip()
 
-     line_parts = line1.split()
-
-     if(len(line_parts) > 2):
+     #if(re.match('^[A-Z0-9]+\s[A-z{1}\(s|\t)+[0-9]+\:',line1)):
+     if(re.search('[0-9]+\:',line1)):
          one_line.append(l)
          l = line1
      else:
-         l = l + ' ' + line1
+         l = l + ';' + line1
 for line in one_line:
-    uniprot_lines.write(line+'\n');
+    uniprot_lines.write(line+'\n')
 
+uniprot_lines.write(line1+'\n')
 
     #A1.append(lines_db)
 #
